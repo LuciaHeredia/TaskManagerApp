c4fd9122ca2665a6011e50892f35c8156d451427
Lucia Heredia <luciaheredia22@gmail.com>
2025-07-26 03:31:55 +0300

    added dirc in path

--- task_manager/upload_to_s3.py
+++ task_manager/upload_to_s3.py
@@ -7,7 +7,7 @@ bucket_name = os.getenv("S3_BUCKET_NAME")
 region = os.getenv("AWS_REGION")
 
 # Find the PDF in the 'reports' folder
-reports_folder = "reports"
+reports_folder = "task_manager/reports"
 pdf_files = [f for f in os.listdir(reports_folder) if f.endswith(".pdf")]
 
 if not pdf_files:
@@ -16,7 +16,7 @@ if not pdf_files:
 pdf_file = pdf_files[0]
 pdf_path = os.path.join(reports_folder, pdf_file)
 
-# Upload to S3 with full key: e.g. reports/filename_datetime.pdf
+# Upload to S3 with full key: e.g. task_manager/reports/filename_datetime.pdf
 current_datetime = datetime.today().strftime('%d-%m-%Y_%H-%M-%S') # day-month-year_hour-min-sec
 s3 = boto3.client("s3", region_name=region)
 s3.upload_file(pdf_path, bucket_name, f"{reports_folder}/{pdf_file}_{current_datetime}")
